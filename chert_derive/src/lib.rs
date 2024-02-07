06067c354ef91d995d6fd050e585e7c09e87fe8d
Stephen Bennett <spb@libera.chat>
2024-02-07 05:35:21 +0000

    Allow accessing input fields via AsRef<_>

--- chert_derive/src/lib.rs
+++ chert_derive/src/lib.rs
@@ -1,14 +1,37 @@
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
-use quote::quote;
-use syn::{parse_macro_input, Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed};
+use quote::{quote, ToTokens};
+use syn::{parse::Parse, parse_macro_input, Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed, Token, Type};
 
-#[proc_macro_derive(ChertStruct)]
+mod kw {
+    syn::custom_keyword!(as_ref);
+}
+
+enum ChertAttribute {
+    AsRef {
+        _as_ref: kw::as_ref,
+        _equals: Token![=],
+        as_type: Type,
+    }
+}
+
+impl Parse for ChertAttribute {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(kw::as_ref) {
+            Ok(Self::AsRef { _as_ref: input.parse()?, _equals: input.parse()?, as_type: input.parse()? })
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+#[proc_macro_derive(ChertStruct, attributes(chert))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, data, .. } = parse_macro_input!(input as DeriveInput);
+    let DeriveInput { ident: struct_name, data, .. } = parse_macro_input!(input as DeriveInput);
 
     let Struct(DataStruct {
-        fields: Named(FieldsNamed { ref named, .. }),
+        fields: Named(FieldsNamed { named: ref named_fields, .. }),
         ..
     }) = data
     else {
@@ -18,35 +41,58 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut fields = Vec::new();
     let mut accessor_functions = Vec::new();
 
-    for (i, t) in named
-        .iter()
-        .filter_map(|f| f.ident.as_ref().map(|i| (i, &f.ty)))
+    for field in named_fields.iter()
     {
+        let Some(field_name) = &field.ident else { continue };
+        let mut field_type = field.ty.clone();
+        let mut use_as_ref = false;
+
+        for attr in &field.attrs {
+            if attr.path().is_ident("chert") {
+                let Ok(chert_attr)  = attr.parse_args::<ChertAttribute>() else { panic!("Invalid chert attribute: {}", attr.to_token_stream()) };
+
+                let ChertAttribute::AsRef { as_type, .. } = chert_attr;
+                field_type = as_type;
+                use_as_ref = true;
+            }
+        }
+
         let accessor_name = Ident::new(
-            &format!("_chert_get_{}", i.to_string().to_ascii_lowercase()),
-            i.span(),
+            &format!("_chert_get_{}", field_name.to_string().to_ascii_lowercase()),
+            field_name.span(),
         );
 
-        let ident_str = i.to_string();
+        let ident_str = field_name.to_string();
 
         fields.push(quote! {
-            (#ident_str, <#t as chert::ChertFieldType>::from_field(Self::#accessor_name))
+            (#ident_str, <#field_type as chert::ChertFieldType>::from_field(Self::#accessor_name))
         });
 
-        accessor_functions.push(quote! {
-            #[allow(non_snake_case)]
-            fn #accessor_name(object: &#ident) -> &<#t as chert::ChertFieldType>::AccessedAs {
-                &object.#i
-            }
-        });
+        if use_as_ref {
+            accessor_functions.push(quote! {
+                #[allow(non_snake_case)]
+                fn #accessor_name(object: &#struct_name) -> &<#field_type as chert::ChertFieldType>::AccessedAs {
+                    use std::convert::AsRef;
+                    object.#field_name.as_ref()
+                }
+            });
+
+        } else {
+            accessor_functions.push(quote! {
+                #[allow(non_snake_case)]
+                fn #accessor_name(object: &#struct_name) -> &<#field_type as chert::ChertFieldType>::AccessedAs {
+                    &object.#field_name
+                }
+            });
+        }
     }
 
     quote! {
-        impl #ident {
+        impl #struct_name {
             #(#accessor_functions)*
         }
 
-        impl chert::ChertStructTrait for #ident {
+        impl chert::ChertStructTrait for #struct_name {
             fn fields() -> std::collections::HashMap<String, (usize, chert::ChertField<Self>)> {
                 use std::collections::HashMap;
                 use chert::ChertField;
