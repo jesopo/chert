b112fbb4073c3c6d0477b21bd192e4dc5ba54e37
jesopo <github@lolnerd.net>
2024-02-07 22:25:15 +0000

    additional "unsafe" test for serialising with IDs

--- tests/only-slightly-unsafe.rs
+++ tests/only-slightly-unsafe.rs
@@ -1,5 +1,5 @@
 #[test]
-fn test_regex() {
+fn test_serialize() {
     #[derive(chert::ChertStruct, Debug)]
     struct Variables {
         a: u64,
@@ -10,5 +10,20 @@ fn test_regex() {
 
     let ast: chert::NodeBoolean = serde_json::from_str(&ast).unwrap();
     let engine = unsafe { chert::compile_unsafe::<Variables, _>(Vec::from([(0, ast)])) };
+    engine.eval(&Variables { a: 1 });
+}
+
+#[test]
+fn test_serialize_with_id() {
+    #[derive(chert::ChertStruct, Debug)]
+    struct Variables {
+        a: u64,
+    }
+
+    let ast = chert::parse::<Variables>("a == 1").unwrap();
+    let ast = serde_json::to_string_pretty(&Vec::from([(0, ast.root)])).unwrap();
+
+    let asts: Vec<(i32, chert::NodeBoolean)> = serde_json::from_str(&ast).unwrap();
+    let engine = unsafe { chert::compile_unsafe::<Variables, _>(asts) };
     engine.eval(&Variables { a: 1 });
 }
